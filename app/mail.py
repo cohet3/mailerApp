49f8095510ec8a86790b13abda07cc454ecfba99
cohet3 <tauro.total90@gmail.com>
2023-02-04 18:18:39 +0100

    agregando la ruta para crear emails

---
+++ app/mail.py
@@ -0,0 +1,22 @@
+from flask import ( 
+    Blueprint, render_template
+)
+from app.db import get_db
+
+
+bp = Blueprint('mail', __name__, url_prefix="/")
+
+@bp.route('/', methods=['GET'])
+def index():
+    db, c = get_db()
+
+    c.execute("SELECT * FROM email")
+    mails = c.fetchall()
+    
+  
+
+    return render_template('mails/index.html', mails=mails)
+
+@bp.route('/create', methods=['GET', 'POST'])
+def create():
+    return render_template('mails/create.html')
