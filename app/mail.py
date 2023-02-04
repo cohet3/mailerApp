8f2bf3b1c6fae0a83832b48cac4d63ffdbb601bd
cohet3 <tauro.total90@gmail.com>
2023-02-04 19:45:23 +0100

    listo antes del sendgrid

--- app/mail.py
+++ app/mail.py
@@ -1,5 +1,5 @@
 from flask import ( 
-    Blueprint, render_template
+    Blueprint, render_template, request, flash, url_for, redirect
 )
 from app.db import get_db
 
@@ -19,4 +19,29 @@ def index():
 
 @bp.route('/create', methods=['GET', 'POST'])
 def create():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        content = request.form.get('content')
+        errors = []
+
+        if not email:
+            errors.append('Email es obligatorio')
+        if not subject:
+            errors.append('Asunto es obligatorio')
+        if not content:
+            errors.append('Contenido es obligatorio')
+
+        if len(errors) == 0:
+            db, c = get_db()
+            c.execute("INSERT INTO email (email, subject, content) VALUES (%s,%s,%s)", (email, subject, content))
+            db.commit()
+
+            return redirect(url_for('mail.index'))
+        else:
+            for error in errors:
+                flash(error)
     return render_template('mails/create.html')
+
+    def send(to, subject, content):
+        sg=sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
